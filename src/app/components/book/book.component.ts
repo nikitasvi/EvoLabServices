7cd11d0b449854bc5dbaed54dafbeda5b192edc9
nikita-maltsev <nikitasvi1000@gmail.com>
2023-09-20 17:29:20 +0300

    cleanup #2

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BooksService } from 'src/app/services/books.service';
 
 @Component({
@@ -6,8 +6,6 @@ import { BooksService } from 'src/app/services/books.service';
   templateUrl: './book.component.html',
   styleUrls: ['./book.component.css'],
 })
-export class BookComponent implements OnInit {
+export class BookComponent {
   constructor(public bookService: BooksService) {}
-
-  ngOnInit() {}
 }
